7f3eb2359877a32b7d0697581e8da931f4e50c8f
Taharia Begum <taharia@Taharias-MacBook-Pro-2.local>
2023-11-12 22:40:02 -0500

    commit11

--- script.js
+++ script.js
@@ -4,7 +4,19 @@
 // alert winner 
 // have reset button clear board 
 
-const pieces = document.querySelectorAll(".piece");
-for (let piece of pieces) {
-    piece.addEventListener("click", (evt) => {piece.style.backgroundColor = "red"})
-}
+// const pieces = document.querySelectorAll(".piece");
+// for (let piece of pieces) {
+//     piece.addEventListener("click", (evt) => {piece.style.backgroundColor = "red"})
+// }
+
+
+//select player (red and yellow) --> alternate turns 
+//pieces gravitate toward lowest open position 
+//click changes color -- red/yellow alternating 
+//check winner before placing
+    //check horizontal
+    //check vertical 
+    //check diagonal 
+//if winner, alert ___ is winner and stop game
+//if no open positions available -- alert tie and restart
+//option to restart game (restart button clears board)
