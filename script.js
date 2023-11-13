353101a79c447cc02f7a60378eb9be213b42c740
Taharia Begum <taharia@Taharias-MacBook-Pro-2.local>
2023-11-12 22:35:49 -0500

    ten

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 // have pieces start bottom and work way up
 // winning combinations 
 // alert winner 
+// have reset button clear board 
 
+const pieces = document.querySelectorAll(".piece");
+for (let piece of pieces) {
+    piece.addEventListener("click", (evt) => {piece.style.backgroundColor = "red"})
+}
