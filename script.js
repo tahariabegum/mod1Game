1e9b922f908cf137fc3012f8c4a1427195554d30
Taharia Begum <taharia@Taharias-MacBook-Pro-2.local>
2023-11-15 19:10:11 -0500

    next commit

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-
-// have pieces start bottom and work way up
-// winning combinations 
-// alert winner 
-//pieces gravitate toward lowest open position 
-//check winner before placing
-    //check horizontal
-    //check vertical 
-    //check diagonal 
-//if winner, alert ___ is winner and stop game --> gameOver == true 
-//if no open positions available -- alert tie and restart
-
-const column = 7;
-const row = 6;
+const columns = 7;
+const rows = 6;
 let currentPlayer = "red"
 let gameOver = false
 
@@ -19,11 +7,44 @@ let gameOver = false
 const reset = document.querySelector(".resetButton");
 const pieces = document.querySelectorAll(".piece");
 
-
-
-
 //Brings piece to first opening on bottom of board 
 
+//Changes color of background 
+// for (let piece of pieces) {
+//     piece.addEventListener ("click", () => {
+//         piece.style.backgroundColor = currentPlayer;
+//         turn()
+//     });
+//     reset.addEventListener("click", () => {piece.style.backgroundColor = "", currentPlayer = "red"}); //Resets game 
+// }
+
+//Outer Loop through column 
+for (let col = 0; col < columns; col++) {
+    const columnPiece = document.querySelectorAll(`.piece:nth-child(${col + 1})`); 
+    //Inner Loop through rows in current columns 
+    for (let row = 0; row < rows; row++) {
+        const piece = columnPiece[row]
+        for (let piece of pieces) {
+        piece.addEventListener("click", () => {
+        //checks game over -- if not over --> find empty row in current column 
+            if(!gameOver) {
+                const currentRow = emptyRow(col) //emptyRow in clicked column 
+                //If valid move is possible, change background to player piece 
+                if (currentRow !== -1) {
+                    piece.style.backgroundColor = currentPlayer;
+                    //check if winner 
+                    checkWinner(currentRow, col)
+                    //Next player turn 
+                    turn()
+                }
+            }
+            
+        });
+        
+    }
+
+}}
+    
 //Alternates Players
 function turn() {
     if (currentPlayer === "red") { 
@@ -32,14 +53,27 @@ function turn() {
         currentPlayer = "red"
     }
 }
-//Changes color of background 
-for (let piece of pieces) {
-    piece.addEventListener ("click", () => {
-        piece.style.backgroundColor = currentPlayer;
-        turn()
-    });
-    reset.addEventListener("click", () => {piece.style.backgroundColor = "", currentPlayer = "red"}); //Resets game 
+
+//Find empty row in column 
+function emptyRow(col) {
+    for (let row = rows - 1; row >= 0; row--) {
+        const indexPiece = col + row * columns ;
+        if (!pieces[indexPiece].style.backgroundColor){
+            return row
+        }
+            
+    }
+    return -1
 }
+
+reset.addEventListener("click", () => {
+    for (let piece of pieces) {
+        piece.style.backgroundColor = "";
+        currentPlayer = "red";
+        gameOver = false;
+    }
+}) 
+
 
 // function dropPiece(index) {
 //     const columns =  document.querySelectorAll('.board .piece');
